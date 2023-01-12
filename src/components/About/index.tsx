bbafbbbc73433a729adf1c5477afc6e3dc48e14e
Bhargav Tenali <t.bhargav@kayrros.com>
2023-01-12 15:05:26 +0530

    minor fixes

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -33,47 +33,57 @@ const About = () => {
           </div>
           <div className="column is-2"></div>
           <div className="column is-5">
-            <p className="is-size-5 has-text-weight-semibold has-text-centered mb-6 has-text-grey">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-              adipisci dicta eveniet iure sapiente animi placeat in error ut
-              saepe.
-            </p>
-            <div className="is-flex is-flex-direction-row is-justify-content-space-around mb-6">
-              <div className="has-text-centered">
-                <h1 className="is-size-3 has-text-weight-bold">01+</h1>
-                <p className="is-size-6 has-text-weight-bold has-text-grey">
-                  Years <br /> experience
-                </p>
+            <div
+              style={{ height: "100%" }}
+              className="is-flex is-flex-direction-column is-justify-content-center is-align-items-stretch"
+            >
+              <p className="is-size-5 has-text-weight-semibold has-text-centered mb-6 has-text-grey">
+                I’m currently working as Full Stack Developer <br />
+                I’m looking to collaborate on Frontend and Backend
+              </p>
+              <div className="is-flex is-flex-direction-row is-justify-content-space-around mb-6">
+                <div className="has-text-centered">
+                  <h1 className="is-size-3 has-text-weight-bold">01+</h1>
+                  <p className="is-size-6 has-text-weight-bold has-text-grey">
+                    Years <br /> Web Development Exp.
+                  </p>
+                </div>
+                <div className="has-text-centered">
+                  <h1 className="is-size-3 has-text-weight-bold">04+</h1>
+                  <p className="is-size-6 has-text-weight-bold has-text-grey">
+                    Years <br /> Managerial Exp.
+                  </p>
+                </div>
+                <div className="has-text-centered">
+                  <h1 className="is-size-3 has-text-weight-bold ">30+</h1>
+                  <p className="is-size-6 has-text-weight-bold has-text-grey">
+                    Completed <br /> Web Projects
+                  </p>
+                </div>
+                <div className="has-text-centered">
+                  <h1 className="is-size-3 has-text-weight-bold ">20+</h1>
+                  <p className="is-size-6 has-text-weight-bold has-text-grey">
+                    Web Development <br /> Certifications
+                  </p>
+                </div>
               </div>
-              <div className="has-text-centered">
-                <h1 className="is-size-3 has-text-weight-bold ">30+</h1>
-                <p className="is-size-6 has-text-weight-bold has-text-grey">
-                  Completed <br /> Projects
-                </p>
+              <div className="is-flex is-flex-direction-row is-justify-content-center is-align-items-center">
+                <a href={Resume} download="Bhargav-Resume" target="_blank">
+                  <button
+                    className="is-size-5 button is-align-self-flex-start is-rounded"
+                    style={{
+                      backgroundColor: "#705edc",
+                      color: "white",
+                      borderRadius: "10px",
+                    }}
+                  >
+                    <span>Download CV</span>
+                    <span className="icon">
+                      <i className="ri-download-2-line"></i>
+                    </span>
+                  </button>
+                </a>
               </div>
-              <div className="has-text-centered">
-                <h1 className="is-size-3 has-text-weight-bold ">20+</h1>
-                <p className="is-size-6 has-text-weight-bold has-text-grey">
-                  Web Development <br /> Certifications
-                </p>
-              </div>
-            </div>
-            <div className="is-flex is-flex-direction-row is-justify-content-center is-align-items-center">
-              <a href={Resume} download="Bhargav-Resume" target="_blank">
-                <button
-                  className="is-size-5 button is-align-self-flex-start is-rounded"
-                  style={{
-                    backgroundColor: "#705edc",
-                    color: "white",
-                    borderRadius: "10px",
-                  }}
-                >
-                  <span>Download CV</span>
-                  <span className="icon">
-                    <i className="ri-download-2-line"></i>
-                  </span>
-                </button>
-              </a>
             </div>
           </div>
         </div>
